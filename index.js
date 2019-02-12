a935a3300a41ed339057a9b633704ac9146a014a
Felice Forby <feliceforby@gmail.com>
2019-02-12 06:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ class Passenger {
 
     store.passengers.push(this);
   }
+
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.passengerId === this.id;
+      }.bind(this)
+    );
+  }
 }
 
 let tripId = 0;
